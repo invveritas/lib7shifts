4b43e617faf5640993a48a7e985c0e228aacade8
Gerad Coles <gerad@prairiedogbrewing.ca>
2019-06-23 03:47:32 -0600

    overload datetime class for friendlier string repr of datetime

--- lib7shifts/dates.py
+++ lib7shifts/dates.py
@@ -5,15 +5,20 @@ import datetime
 
 DEFAULT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
+class DateTime7Shifts(datetime.datetime):
+    """Override representation of dates in datetime objects to match 7shifts form"""
+    def __str__(self):
+        return self.strftime(DEFAULT_DATETIME_FORMAT)
+
 def to_datetime(date_string):
     """Given a date string in API format, return a :class:`datetime.datetime`
     object corresponding to the date and time"""
-    return datetime.datetime.strptime(date_string, DEFAULT_DATETIME_FORMAT)
+    return DateTime7Shifts.strptime(date_string, DEFAULT_DATETIME_FORMAT)
 
 def from_datetime(dt_obj):
     """Converts the datetime object back into a text representation compatible
     with the 7shifts API"""
-    return dt_obj.strftime(DEFAULT_DATETIME_FORMAT)
+    return dt_obj.__str__()
 
 def to_y_m_d(dt_obj):
     """Converts a datetime object to text in YYYY-MM-DD format"""
