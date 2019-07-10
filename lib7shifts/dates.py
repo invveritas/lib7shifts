cba573c6de09b432d05202f3a9f2c92556464e2d
Gerad Coles <gerad@prairiedogbrewing.ca>
2019-07-10 04:55:18 -0600

    Better support for local timestamps

--- lib7shifts/dates.py
+++ lib7shifts/dates.py
@@ -4,27 +4,53 @@ Utilities for handling dates from the 7Shifts API
 import datetime
 
 DEFAULT_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
 
 class DateTime7Shifts(datetime.datetime):
-    """Override representation of dates in datetime objects to match 7shifts form"""
+    """Override representation of dates in datetime objects to match
+    7shifts form"""
+
     def __str__(self):
         return self.strftime(DEFAULT_DATETIME_FORMAT)
 
-def to_datetime(date_string):
+
+def get_local_tz():
+    "Return the current local timezone"
+    return datetime.datetime.utcnow().astimezone().tzinfo
+
+
+def to_datetime(date_string, tz=datetime.timezone.utc):
+    """Given a datetime string in API format, return a
+    :class:`datetime.datetime` object corresponding to the date and time"""
+    date = DateTime7Shifts.strptime(
+        date_string, DEFAULT_DATETIME_FORMAT)
+    return date.replace(tzinfo=tz)
+
+
+def to_date(date_string, tz=datetime.timezone.utc):
     """Given a date string in API format, return a :class:`datetime.datetime`
     object corresponding to the date and time"""
     date = DateTime7Shifts.strptime(
-        date_string, DEFAULT_DATETIME_FORMAT)
-    return date.replace(tzinfo=datetime.timezone.utc)
+        date_string, DEFAULT_DATE_FORMAT)
+    return date.replace(tzinfo=tz)
+
+
+def get_epoch_ts_for_date(date):
+    "Given a local date of form YYYY-MM-DD, return a unix TS"
+    return to_date(date, tz=get_local_tz()).timestamp()
+
 
 def from_datetime(dt_obj):
     """Converts the datetime object back into a text representation compatible
     with the 7shifts API"""
     return dt_obj.__str__()
 
+
 def to_y_m_d(dt_obj):
     """Converts a datetime object to text in YYYY-MM-DD format"""
     return dt_obj.strftime("%Y-%m-%d")
+
 
 def to_h_m_s(dt_obj):
     """Outputs just the time-portion of a datetime object in HH:MM:SS form"""
