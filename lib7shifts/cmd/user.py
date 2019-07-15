878a10767c647418fb597551eed21423681484f4
Gerad Coles <gerad@prairiedogbrewing.ca>
2019-07-15 14:12:13 -0600

    Fix bad scoping

--- lib7shifts/cmd/user.py
+++ lib7shifts/cmd/user.py
@@ -63,11 +63,11 @@ def get_user(user_id):
 def get_users(args, page_size=200, skip_admin=False):
     """Get a list of users from the 7shifts API"""
     client = get_7shifts_client()
+    results = 0
     for active in (1, 0):
         if not args.get('--with-inactive', False):
             break
         offset = 0
-        results = 0
         while True:
             LOG.debug(
                 "getting up to %d users (active: %d) at offset %d",
