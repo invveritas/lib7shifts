bfb61078279d17ef57574f13eb46ebfc6fcd6121
Gerad Coles <gerad@prairiedogbrewing.ca>
2019-09-28 03:17:20 -0600

    Add deep scan support for users and get role info.

--- lib7shifts/users.py
+++ lib7shifts/users.py
@@ -8,13 +8,18 @@ from .dates import to_local_date
 ENDPOINT = '/v1/users'
 
 
-def get_user(client, user_id):
+def get_user(client, user_id, **urlopen_kw):
     """Implements the 'Read' API in 7Shifts for the given `user_id`.
     Returns a :class:`User` object, or raises
-    :class:`exceptions.EntityNotFoundError` if the user wasn't found."""
-    response = client.read(ENDPOINT, user_id)
+    :class:`exceptions.EntityNotFoundError` if the user wasn't found.
+    For full user information, pass the following for **urlopen_kw:
+
+        {'deep': 1}
+
+    """
+    response = client.read(ENDPOINT, user_id, **urlopen_kw)
     try:
-        return User(**response['data']['user'], client=client)
+        return User(**response['data'], client=client)
     except KeyError:
         raise exceptions.EntityNotFoundError('User', user_id)
 
@@ -28,7 +33,7 @@ def list_users(client, **kwargs):
 
     - limit: limit the number of results to be returned
     - offset: return results starting from an offset
-    - order_field: the field to order results by, eg: order_field=shift.modified
+    - order_field: a field to order results by, eg: order_field=shift.modified
     - order_dir: "asc" or "desc"
 
     Returns a :class:`UserList` object containing :class:`User` objects.
@@ -47,6 +52,14 @@ class User(base.APIObject):
         super(User, self).__init__(**kwargs)
         self._company = None
 
+    def __getattr__(self, name):
+        """
+        Because this class is passed a nested dictionary, the parent's
+        behaviour needs to be overloaded to look deeper in the dict to find its
+        attributes.
+        """
+        return self._api_data('user')[name]
+
     @property
     def punch_code(self):
         """
@@ -60,7 +73,8 @@ class User(base.APIObject):
         return self.employee_id
 
     def is_employee(self):
-        "Returns True if the user_type_id is 1 (employee) - doesn't include mgmt"
+        """Returns True if the user_type_id is 1 (employee) - doesn't include
+        managers"""
         if self.user_type_id == 1:
             return True
         return False
@@ -97,13 +111,81 @@ class User(base.APIObject):
 
     def get_company(self):
         """Return a :class:`lib7shfits.companies.Company` object
-        associated with this shift.
-        An API fetch will be used to fetch this data (once)"""
+        associated with this user. If the initial request to get this user
+        included the `deep` parameter, set to 1, the API data returned from
+        that request will be used to populate the company object, avoiding a
+        second API fetch. Otherwise, another API fetch must be used to populate
+        company data. As such, the richness of company data may not be the
+        same between the two methods."""
         if self._company is None:
             from . import companies
-            self._company = companies.get_company(
-                self.company_id, self.client)
+            try:
+                self._company = companies.Company(
+                    **self._api_data('company'), client=self.client)
+            except KeyError:
+                self._company = companies.get_company(
+                    self.client, self.company_id)
         return self._company
+
+    def get_departments(self):
+        """Return an iterable of :class:`lib7shifts.Department` objects that
+        apply to this user.
+
+        IMPORTANT: This method only works if the API call used to fetch this
+        user had the `deep` parameter set to 1.
+        """
+        from . import departments
+        try:
+            for data in self._api_data('department'):
+                yield departments.Department(**data, client=self.client)
+        except KeyError:
+            raise RuntimeError((
+                "Unable to get department information for user without"
+                " deep=1"))
+
+    def get_locations(self):
+        """Return an iterable of :class:`lib7shifts.Location` objects that
+        apply to this user.
+
+        IMPORTANT: This method only works if the API call used to fetch this
+        user had the `deep` parameter set to 1.
+        """
+        from . import locations
+        try:
+            for data in self._api_data('location'):
+                yield locations.Location(**data, client=self.client)
+        except KeyError:
+            raise RuntimeError((
+                "Unable to get location information for user without"
+                " deep=1"))
+
+    def get_permissions(self):
+        """Return a dictionary of permissions settings for this user.
+
+        IMPORTANT: This method only works if the API call used to fetch this
+        user had the `deep` parameter set to 1.
+        """
+        try:
+            return self._api_data('permission')
+        except KeyError:
+            raise RuntimeError((
+                "Unable to get permission information for user without"
+                " deep=1"))
+
+    def get_roles(self):
+        """Return role information for the user, such as the name, department
+        id, etc. A :class:`lib7shifts.RoleList` object will be returned, since
+        users can have more than one role associated with them.
+
+        IMPORTANT: This method only works if the initial call to get this user
+        object included the `deep` parameter, set to 1."""
+        try:
+            from . import roles
+            for data in self._api_data('role'):
+                yield roles.Role(**data, client=self.client)
+        except KeyError:
+            raise RuntimeError(
+                "Unable to get role information for user without deep=1")
 
     def get_wages(self):
         """Returns a dictionary of wage data for the user. This method utilizes
